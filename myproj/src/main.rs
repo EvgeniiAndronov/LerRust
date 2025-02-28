58fdfacb85c94298d2addbf3c8c4e2b95d448cac
Ged <120242971+GedK0tlet@users.noreply.github.com>
2025-02-28 22:44:48 +0300

    init

--- myproj/src/main.rs
+++ myproj/src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
-}
+    let mut inp_val_1 = String::new();
+    let mut inp_val_2: String = String::new();
+
+    println!("Enter num1: ");
+    io::stdin().read_line(&mut inp_val_1).expect("val 1");
+
+    println!("Enter num2: ");
+    io::stdin().read_line(&mut inp_val_2).expect("val 2");
+
+    let data1: i16 = inp_val_1.trim().parse().expect("not valid number");
+    let data2: i16 = inp_val_2.trim().parse().expect("not correct val");
+
+    let result = data1 + data2;
+
+    println!("View:\nn1 = {}, n2 = {};\nn1 + n2 = {}", data1, data2, result);
+} 
